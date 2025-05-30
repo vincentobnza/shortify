24d8c9bb642be4135071d107d80533835736216b
Vincent Obenza <vincentobenza2003@gmail.com>
2025-05-30 11:09:28 +0800

    FIX:

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Ignore TypeScript errors during build
   },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
