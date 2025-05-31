26064326844b63692817b03d827cde4866acc752
Vincent Obenza <vincentobenza2003@gmail.com>
2025-05-31 08:43:58 +0800

    FIX

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://ujvqnzoevjnoawkinypn.supabase.co";
+export const supabaseUrl = "https://ujvqnzoevjnoawkinypn.supabase.co";
 export const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqdnFuem9ldmpub2F3a2lueXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjAxMjcsImV4cCI6MjA2NDA5NjEyN30.3ATSPDwWW0d4E0I8Y8iaKQJUL2BN_EKoG2MP76AJLEY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
